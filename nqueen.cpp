98ed7f0b3687fd35617d9a66bcfc142e5fd64ac2
Nikunj <nikunjvar2002@gmail.com>
2022-07-04 13:58:53 -0700

    nqueenproblems

---
+++ nqueen.cpp
@@ -0,0 +1,74 @@
+//question link
+//https://leetcode.com/problems/n-queens/
+
+
+//solution done by backtracking
+class Solution {
+public:
+    bool issafe(int row,int col,vector<string>& board,int n){
+        int x=row;
+        int y=col;
+        
+        //check for same row
+        while(y>=0){
+            if(board[x][y]=='Q')
+                return false;
+            
+            y--;
+        }
+        x=row;
+        y=col;
+            
+        //check for diagonal
+        while(x>=0 && y>=0){
+            if(board[x][y]=='Q')
+                return false;
+            x--;
+            y--;
+        }
+        x=row;
+        y=col;
+        //check for diagonal
+        while(x<n && y>=0){
+            if(board[x][y]=='Q')
+                return false;
+            x++;
+            y--;
+        }
+        return true;
+    }
+    
+    void solve(int col,  vector<vector<string>>& ans,vector<string>& board,int n){
+        
+        //base case
+        if(col==n){
+           ans.push_back(board);
+            return;
+        }
+        
+        //solve1 case and rest recursion will take care
+        
+        for(int row=0;row<n;row++){
+            if(issafe(row,col,board,n)){
+                
+                //if placing queen is safe
+                board[row][col]='Q';
+                solve(col+1,ans,board,n);
+                //backtrack
+                board[row][col]='.';
+            }
+        }
+    }
+
+    
+    vector<vector<string>> solveNQueens(int n) {
+        
+        vector<string> board(n, string (n, '.'));
+        
+        vector<vector<string>> ans;
+        
+        solve(0,ans,board,n);
+        
+        return ans;
+    }
+};
